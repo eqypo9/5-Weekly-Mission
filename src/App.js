80e587fee179c470a31bfec975b5471ba145454a
eqypo9 <eqypo@naver.com>
2024-05-07 14:19:11 +0900

    feat: week10 template code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { getSampleUser, getFolderInfo, getFolderList } from "../api";
-import SharedPage from "../pages/SharedPage";
-import FolderPage from "../pages/FolderPage";
-
-function useUser() {
-  const [userInfo, setUserInfo] = useState({ email: "", imgUrl: "" });
-  useEffect(() => {
-    async function loadData() {
-      const result = await getSampleUser();
-      if (!result) return;
-
-      const { email, profileImageSource: imgUrl } = result;
-      setUserInfo({ email, imgUrl });
-    }
-    loadData();
-  }, []);
-  return { userInfo };
-}
-
-function useFolder() {
-  const [folderInfo, setFolderInfo] = useState({});
-  useEffect(() => {
-    async function loadData() {
-      const result = await getFolderInfo();
-      if (!result) return;
-
-      const { folder } = result;
-      setFolderInfo(folder);
-    }
-    loadData();
-  }, []);
-  return { folderInfo };
-}
-
-function useFolderList() {
-  const [folderList, setFolderList] = useState({});
-  useEffect(() => {
-    async function loadData() {
-      const result = await getFolderList(1);
-      if (!result) return;
-
-      const { data } = result;
-      setFolderList({ data });
-    }
-    loadData();
-  }, []);
-  return { folderList };
-}
+import "sharing/styles/reset.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SharedPage } from "pages/SharedPage";
+import { FolderPage } from "pages/FolderPage";
 
 function App() {
-  const { userInfo } = useUser();
-  const { folderInfo } = useFolder();
-  const { folderList } = useFolderList();
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/shared"
-          element={<SharedPage userInfo={userInfo} folderInfo={folderInfo} />}
-        />
-        <Route
-          path="/folder"
-          element={<FolderPage userInfo={userInfo} folderList={folderList} />}
-        />
+        <Route path="/shared" element={<SharedPage />} />
+        <Route path="/folder" element={<FolderPage />} />
       </Routes>
     </BrowserRouter>
   );
